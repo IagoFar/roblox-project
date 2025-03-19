575ab58e10ef07c21e5e8c1d32470fa95e6de110
IagoFar <neila.galiza@hotmail.com>
2025-03-19 14:18:59 +0100

    Added a debounce flag

--- src/Client/Game/Scripts/Gun/GunShoot.client.lua
+++ src/Client/Game/Scripts/Gun/GunShoot.client.lua
@@ -3,6 +3,9 @@ local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
 
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+
+Knit:OnStart():await()
+
 local ExplosionService = Knit.GetService("ExplosionService")
 local GunService = Knit.GetService("GunService")
 
@@ -10,37 +13,47 @@ local Player = Players.LocalPlayer
 
 Player.CharacterAdded:Connect(function(character: Model)
     character.ChildAdded:Connect(function(child: Instance)
-        if child:IsA("Tool") then
-            if CollectionService:HasTag(child, "Gun") then
-                child.Equipped:Connect(function()
-                    child.Activated:Connect(function()
-                        
-                        local mouse = Player:GetMouse()
-                        local origin = mouse.UnitRay.Origin
-                        local direction = mouse.UnitRay.Direction * 1000 
+        if child:IsA("Tool") and CollectionService:HasTag(child, "Gun") then
+            local debounce = false
+            child.Equipped:Connect(function()
+                child.Activated:Connect(function()
+                    if debounce then return end
+                    debounce = true
 
-                        local raycastParams = RaycastParams.new()
-                        raycastParams.FilterDescendantsInstances = {character}
-                        raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
+                    local mouse = Player:GetMouse()
+                    local origin = mouse.UnitRay.Origin
+                    local direction = mouse.UnitRay.Direction * 1000
 
-                        local raycastResult = Workspace:Raycast(origin, direction, raycastParams)
+                    local raycastParams = RaycastParams.new()
+                    raycastParams.FilterDescendantsInstances = {character}
+                    raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
 
-                        if raycastResult then
-                            local hitPart: Instance = raycastResult.Instance
-                            local hitPosition = raycastResult.Position
-                            local hitNormal = raycastResult.Normal
+                    local raycastResult = Workspace:Raycast(origin, direction, raycastParams)
 
-                            if CollectionService:HasTag(hitPart, "Explosive") or CollectionService:HasTag(hitPart.Parent, "Explosive") then
-                                ExplosionService:AddBarrelState(hitPart)
-                            elseif hitPart.Parent:FindFirstChildOfClass("Humanoid") then
-                                GunService:DamageCharacter(hitPart.Parent, child)
-                            end
-                        else
-                            print("No hit")
+                    if raycastResult then
+                        local hitPart = raycastResult.Instance
+                        local hitPosition = raycastResult.Position
+                        local hitNormal = raycastResult.Normal
+
+                        if CollectionService:HasTag(hitPart, "Explosive") or CollectionService:HasTag(hitPart.Parent, "Explosive") then
+                            ExplosionService:AddBarrelState(hitPart)
+                        elseif hitPart.Parent:FindFirstChildOfClass("Humanoid") then
+                            GunService:DamageCharacter(hitPart.Parent, child)
                         end
-                    end)
+                    end
+
+                    local fireRate = child:FindFirstChild("FireRate")
+                    if fireRate and fireRate:IsA("IntValue") then
+                        local delay = 60 / fireRate.Value
+                        print("Delay: ", delay)
+                        task.wait(delay)
+                    else
+                        task.wait(1)
+                    end
+
+                    debounce = false
                 end)
-            end
+            end)
         end
     end)
 end)
